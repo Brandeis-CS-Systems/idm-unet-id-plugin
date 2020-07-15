5b160601014524bf48737c979ad95f404e27f2b6
sternj <sternj@brandeis.edu>
2020-07-15 13:01:17 -0400

    Changed order of actions

--- install.sh
+++ install.sh
@@ -6,11 +6,12 @@ SITE_PACKAGES=$(python3 -c 'from sys import version_info as v; print(f"/usr/lib/
 cp schema.d/89-schemathing.ldif /usr/share/ipa/schema.d/
 
 
-ipa-ldap-updater \
-    -S /usr/share/ipa/schema.d/89-schemathing.ldif
+
 
 cp ipaserver/plugins/*.py ${SITE_PACKAGES}/ipaserver/plugins
 python3 -m compileall ${SITE_PACKAGES}/ipaserver/plugins
 
+ipa-ldap-updater \
+    -S /usr/share/ipa/schema.d/89-schemathing.ldif
 
 ipactl restart
