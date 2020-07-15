8bb020fe5e8db9bf211f4b9d32a58742555cc09b
sternj <sternj@brandeis.edu>
2020-07-15 12:45:12 -0400

    Added initial python plugin support

--- install.sh
+++ install.sh
@@ -5,10 +5,12 @@ SITE_PACKAGES=$(python3 -c 'from sys import version_info as v; print(f"/usr/lib/
 
 cp schema.d/89-schemathing.ldif /usr/share/ipa/schema.d/
 
-cp updates/89-schemathing.update /usr/share/ipa/updates
 
 ipa-ldap-updater \
-    -S /usr/share/ipa/schema.d/89-schemathing.ldif \
-    /usr/share/ipa/updates/89-schemathing.update
+    -S /usr/share/ipa/schema.d/89-schemathing.ldif
+
+cp ipaserver/plugins/*.py ${SITE_PACKAGES}/ipaserver/plugins
+python3 -m compileall ${SITE_PACKAGES}/ipaserver/plugins
+
 
 ipactl restart
--- ipaserver/unetuser.py
+++ ipaserver/unetuser.py
@@ -2,8 +2,46 @@ from ipalib import _
 from ipalib.params import Str
 
 from ipa.plugins.baseuser import baseuser
+from ipa.plugins.user import user, user_add, user_mod
 
-# if "unetUser" not in baseuser.possible_objectclasses:
-#     baseuser.possible_objectclasses.append("unetUser")
+if "unetUser" not in baseuser.possible_objectclasses:
+    baseuser.possible_objectclasses.append("unetUser")
 
-# baseuser.default_attributes.extend("unetID")
+unetuser_attributes = ["unetID"]
+baseuser.default_attributes.extend(unetuser_attributes)
+takes_params = (
+    Str('unetid',
+        cli_name="unetid",
+        maxlength=64,
+        label=_("User UNET uid"))
+)
+
+user.takes_params += takes_params
+
+user.managed_permissions.update(
+    {
+        "System: Read UNET ID": {
+            "replaces_global_anonymous_aci": True,
+            "ipapermbindruletype": "all",
+            "ipapermright": {"read", "search", "compare"},
+            "ipapermtargetfilter": ["(objectclass=unetUser)"],
+            "ipapermdefaultattr": set(unetuser_attributes),
+        },
+        
+    }
+)
+
+def useradd_precallback(self, ldap, dn, entry, attrs_list,*keys, **options):
+    entry['objectclass'].append('unetUser')
+    return dn
+
+user_add.register_pre_callback(useradd_precallback)
+
+def usermod_precallback(self, ldap, dn, entry, attrs_list,*keys, **options):
+    if 'objectclass' not in entry.keys():
+        old_entry = ldap.get_entry(dn, ['objectclass'])
+        entry['objectclass'] = old_entry['objectclass']
+    entry['objectclass'].append('customperson')
+    return dn
+
+user_mod.register_pre_callback(usermod_precallback)
