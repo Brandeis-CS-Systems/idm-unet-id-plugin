bcd116081818c7c4e957835cfef28d89efe7d232
sternj <sternj@brandeis.edu>
2020-07-24 17:29:50 -0400

    Changed installation file

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ set -e
 SITE_PACKAGES=$(python3 -c 'from sys import version_info as v; print(f"/usr/lib/python{v.major}.{v.minor}/site-packages")')
 
 cp schema.d/89-schemathing.ldif /usr/share/ipa/schema.d/
-cp updates/89-schemathing.update /usr/share/ipa/updates/89-schemathing.update
 
 mkdir -p -m 755 /usr/share/ipa/ui/js/plugins/unetuser
 cp ui/js/plugins/unetuser/unetuser.js /usr/share/ipa/ui/js/plugins/unetuser/
@@ -13,8 +12,7 @@ cp ui/js/plugins/unetuser/unetuser.js /usr/share/ipa/ui/js/plugins/unetuser/
 cp ipaserver/plugins/*.py ${SITE_PACKAGES}/ipaserver/plugins
 
 ipa-ldap-updater \
-    -S /usr/share/ipa/schema.d/89-schemathing.ldif \
-    /usr/share/ipa/updates/89-schemathing.update
+    -S /usr/share/ipa/schema.d/89-schemathing.ldif
 
 python3 -m compileall ${SITE_PACKAGES}/ipaserver/plugins
 
