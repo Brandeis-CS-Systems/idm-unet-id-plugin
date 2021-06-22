728f972c16199137ec646c0de275d651d0c50ffa
Noah Walden <noahwalden@cs.brandeis.edu>
2021-06-22 11:22:15 -0400

    install.sh: make cp verbose
    Make it more obvious what's occurring during the plugin install.

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ SITE_PACKAGES=$(python3 -c 'from sys import version_info as v; print(f"/usr/lib/
 cp schema.d/89-schemathing.ldif /usr/share/ipa/schema.d/
 
 mkdir -p -m 755 /usr/share/ipa/ui/js/plugins/unetuser
-cp ui/js/plugins/unetuser/unetuser.js /usr/share/ipa/ui/js/plugins/unetuser/
+cp -v ui/js/plugins/unetuser/unetuser.js /usr/share/ipa/ui/js/plugins/unetuser/
 
 
-cp ipaserver/plugins/*.py ${SITE_PACKAGES}/ipaserver/plugins
+cp -v ipaserver/plugins/*.py ${SITE_PACKAGES}/ipaserver/plugins
 
 ipa-ldap-updater \
     -S /usr/share/ipa/schema.d/89-schemathing.ldif
