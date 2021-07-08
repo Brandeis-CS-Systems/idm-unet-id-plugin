85cba972ef56a6f0cdf967bf51ab461c7c4f511d
Noah Walden <noahwalden@cs.brandeis.edu>
2021-07-08 11:48:50 -0400

    Revert ipapermbindruletype to "all"

--- ipaserver/plugins/unetuser.py
+++ ipaserver/plugins/unetuser.py
@@ -7,28 +7,30 @@ from ipaserver.plugins.stageuser import stageuser, stageuser_add, stageuser_mod
 if "unetuser" not in user.possible_objectclasses:
     user.possible_objectclasses.append("unetuser")
 
-unetuser_attributes = ["unetid", "sponsor", "fwdemail", "expectedgraduation", "allowunetreset"]
+unetuser_attributes = [
+    "unetid",
+    "sponsor",
+    "fwdemail",
+    "expectedgraduation",
+    "allowunetreset",
+]
 user.default_attributes.extend(unetuser_attributes)
 stageuser.default_attributes.extend(unetuser_attributes)
 
 takes_params = (
-    Str('unetid?',
-        cli_name="unetid",
-        maxlength=64,
-        label=_("User UNET uid")),
-    Str('sponsor?',
-        cli_name="sponsor",
-        maxlength=64,
-        label=_("Sponsor")),
-    Bool('fwdemail?',
-        cli_name="fwdemail",
-        label="Forward email?"),
-    Int('expectedgraduation?',
-        cli_name='expectedgraduation',
-        label=_("Expected graduation year")),
-    Bool('allowunetreset?',
-        cli_name='allowunetreset',
-        label=_("Allow reset with UNET ID"))
+    Str("unetid?", cli_name="unetid", maxlength=64, label=_("User UNET uid")),
+    Str("sponsor?", cli_name="sponsor", maxlength=64, label=_("Sponsor")),
+    Bool("fwdemail?", cli_name="fwdemail", label="Forward email?"),
+    Int(
+        "expectedgraduation?",
+        cli_name="expectedgraduation",
+        label=_("Expected graduation year"),
+    ),
+    Bool(
+        "allowunetreset?",
+        cli_name="allowunetreset",
+        label=_("Allow reset with UNET ID"),
+    ),
 )
 user.takes_params += takes_params
 stageuser.takes_params += takes_params
@@ -36,34 +38,34 @@ stageuser.takes_params += takes_params
 read_unet_id_permission = {
     "System: Read UNET ID": {
         "replaces_global_anonymous_aci": True,
-        # "ipapermbindruletype": "all",
-        "ipapermbindruletype": "anonymous",
+        "ipapermbindruletype": "all",
+        # "ipapermbindruletype": "anonymous",
         "ipapermright": {"read", "search", "compare"},
         "ipapermtargetfilter": ["(objectclass=unetuser)"],
-        "ipapermdefaultattr": {
-            "unetid",
-            "sponsor",
-            "fwdemail",
-            "expectedgraduation",
-            "allowunetreset"
-        },
+        "ipapermdefaultattr": set(unetuser_attributes),
     },
 }
 user.managed_permissions.update(read_unet_id_permission)
 # stageuser.managed_permissions.update(read_unet_id_permission)
 
-def useradd_precallback(self, ldap, dn, entry, attrs_list,*keys, **options):
-    entry['objectclass'].append('unetuser')
+
+def useradd_precallback(self, ldap, dn, entry, attrs_list, *keys, **options):
+    entry["objectclass"].append("unetuser")
     return dn
+
+
 user_add.register_pre_callback(useradd_precallback)
 stageuser_add.register_pre_callback(useradd_precallback)
 
-def usermod_precallback(self, ldap, dn, entry, attrs_list,*keys, **options):
-    if 'objectclass' not in entry.keys():
-        old_entry = ldap.get_entry(dn, ['objectclass'])
-        entry['objectclass'] = old_entry['objectclass']
-    if not self.obj.has_objectclass(entry["objectclass"], 'unetuser'):
-        entry['objectclass'].append('unetuser')
+
+def usermod_precallback(self, ldap, dn, entry, attrs_list, *keys, **options):
+    if "objectclass" not in entry.keys():
+        old_entry = ldap.get_entry(dn, ["objectclass"])
+        entry["objectclass"] = old_entry["objectclass"]
+    if not self.obj.has_objectclass(entry["objectclass"], "unetuser"):
+        entry["objectclass"].append("unetuser")
     return dn
+
+
 user_mod.register_pre_callback(usermod_precallback)
 stageuser_mod.register_pre_callback(usermod_precallback)
