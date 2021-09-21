1874514bd149ba582dacd2278eb1ac7f86801be9
Noah Walden <noahwalden@cs.brandeis.edu>
2021-09-21 16:36:54 -0400

    Move chmod to correct place; chmod JS files
    Fix placement of Python plugin chmod and perform the same operation on
    the JS file so the web interface will load properly.

--- install.sh
+++ install.sh
@@ -6,11 +6,12 @@ SITE_PACKAGES=$(python3 -c 'from sys import version_info as v; print(f"/usr/lib/
 cp -v schema.d/89-schemathing.ldif /usr/share/ipa/schema.d/
 mkdir -p -m 755 /usr/share/ipa/ui/js/plugins/brandeiscosciperson
 cp -v ui/js/plugins/brandeiscosciperson/brandeiscosciperson.js /usr/share/ipa/ui/js/plugins/brandeiscosciperson/
-chmod 644 /usr/lib/python3.6/site-packages/ipaserver/plugins/brandeiscosciperson.py
+chmod 644 /usr/share/ipa/ui/js/plugins/brandeiscosciperson/brandeiscosciperson.js
 
 cp -v ipaserver/plugins/*.py ${SITE_PACKAGES}/ipaserver/plugins
 chcon system_u:object_r:lib_t:s0 \
     /usr/lib/python3.6/site-packages/ipaserver/plugins/brandeiscosciperson.py
+chmod 644 /usr/lib/python3.6/site-packages/ipaserver/plugins/brandeiscosciperson.py
 
 ipa-ldap-updater -S /usr/share/ipa/schema.d/89-schemathing.ldif
 python3 -m compileall ${SITE_PACKAGES}/ipaserver/plugins
